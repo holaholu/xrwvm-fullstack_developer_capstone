1d1d02140969a2e8f8ddd2fe5c516e94fc3b87da
holaholu <olaoluhimself@yahoo.com>
2025-03-19 03:34:28 -0400

    lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -79,7 +79,7 @@ def get_cars(request):
     print(count)
     if (count == 0):
         initiate()
-    car_models = CarModel.objects
+    car_models = CarModel.objects\
     .select_related('car_make')
     cars = []
     for car_model in car_models:
